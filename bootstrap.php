d9862912b49dbc2293faad1c4c71a940a5806b66
izejuy <46389306+izejuy@users.noreply.github.com>
2019-01-05 17:14:10 -0500

    Create bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+/**
+ * Gem Chess - A better website for playing chess
+ *
+ * @author Gem Chess Contributors <https://github.com/izejuy/gem-chess/graphs/contributors>.
+ *
+ * @link <https://github.com/izejuy/gem-chess> Source.
+ */
+
+use Symfony\Component\Templating\PhpEngine;
+use Symfony\Component\Templating\TemplateNameParser;
+use Symfony\Component\Templating\Loader\FilesystemLoader;
+
+/**
+ * Render a template to client-side.
+ *
+ * @param string $template The template name.
+ * @param array  $bindData The bind data for template.
+ *
+ * @return void Returns nothing.
+ */
+function render(string $template, array $bindData = []): void
+{
+    $filesystemLoader = new FilesystemLoader(__DIR__ . '/lib/views/%name%');
+    $templating = new PhpEngine(new TemplateNameParser(), $filesystemLoader);
+    echo $templating->render($template, $bindData);
+}

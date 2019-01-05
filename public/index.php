9fa62cc8bfd24ebaeb013678ada2c58fc7f16718
izejuy <46389306+izejuy@users.noreply.github.com>
2019-01-05 16:50:54 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,17 @@
+<?php
+declare(strict_types=1);
+/**
+ * Gem Chess - A better website for playing chess
+ *
+ * @author Gem Chess Contributors <https://github.com/izejuy/gem-chess/graphs/contributors>.
+ *
+ * @link <https://github.com/izejuy/gem-chess> Source.
+ */
 
+use Symfony\Component\Debug\Debug;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+if ($_ENV['DEBUG']) {
+    Debug::enable();
+}

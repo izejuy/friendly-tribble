2b0b9ed21f76f56a256c263bbddef6a51728721c
izejuy <46389306+izejuy@users.noreply.github.com>
2019-01-06 18:15:53 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,19 +5,5 @@ declare(strict_types=1);
  * @link <https://github.com/izejuy/gem-chess> Source.
  */
 
-use Symfony\Component\Debug\Debug;
-use Symfony\Component\Dotenv\Dotenv;
-
 // Require the package dependencies.
 require_once __DIR__ . '/../vendor/autoload.php';
-
-// Load env config.
-(new Dotenv())->load(__DIR__ . '/../gem.env');
-
-// Enable debug if requested by env config.
-if ($_ENV['DEBUG']) {
-    Debug::enable();
-}
-
-// Require the bootstrap file for additional code.
-require_once __DIR__ . '/../bootstrap.php';

2b2d645380f93f6320cdd56546bfaf89241921d2
izejuy <46389306+izejuy@users.noreply.github.com>
2019-01-07 02:36:53 -0500

    Delete style.html.php

--- lib/views/partials/style.html.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-<link href="https://stackpath.bootstrapcdn.com/bootswatch/4.2.1/superhero/bootstrap.min.css" rel="stylesheet" integrity="sha384-3C9izce+NThLmHHj0Gt0VSOFFr0FCxoyLNcCB+uYxTGXDdw64SxJ34SI/rnKRnvu" crossorigin="anonymous">  
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">

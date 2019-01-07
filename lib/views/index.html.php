c14bf7bcc412719569259e947f3d21079ab76986
izejuy <46389306+izejuy@users.noreply.github.com>
2019-01-07 02:34:18 -0500

    Update index.html.php

--- lib/views/index.html.php
+++ lib/views/index.html.php
@@ -1,12 +1,3 @@
 <?php
 $view['slots']->set('title', 'Gem Chess - A better playing chess website powered by Symfony.');
-?>
-<!DOCTYPE html>
-<html lang="%">
-    <?php $view->extend('/partials/head.html.php') ?>
-    <body>
-        <h1>Hello world!</h1>
-        
-        <?php $view->extend('/partials/script.html.php') ?>
-    </body>
-</html>
+$view->extend('/partials/base.html.php');

dd79615019f9528bda6e1085233ec4b1fec3c535
izejuy <46389306+izejuy@users.noreply.github.com>
2019-01-07 03:08:14 -0500

    Update and rename index.html.php to login.html.php

---
+++ lib/views/login.html.php
@@ -0,0 +1,4 @@
+<?php
+$view['slots']->set('title', 'Gem Chess - A better playing chess website powered by Symfony.');
+$view->extend('/layouts/base.html.php');
+?><main role="main" class="flex-shrink-0"><br /><br /><center><div class="container"><a href="./"><h1 class="display-4"><i class="fas fa-gem gem-color" style="color:#2ecc71;"></i></h1></a><h1 class="display-4">Gem Login</h1><p class="lead">Please authenticate yourself!</p></div></main><footer class="footer mt-auto py-3"><center><div class="container"><span class="text-muted">Copyright (c) 2019 Gem Chess</span></div></footer>

dc7e5834667c8b09f35f3ec762c1c2ecee617a08
izejuy <46389306+izejuy@users.noreply.github.com>
2019-01-07 02:51:24 -0500

    Update LoginController.php

--- controllers/Auth/LoginController.php
+++ controllers/Auth/LoginController.php
@@ -1,1 +1,27 @@
+<?php
+declare(strict_types=1);
+/**
+ * @author Gem Chess Contributors <https://github.com/izejuy/gem-chess/graphs/contributors>.
+ * @link <https://github.com/izejuy/gem-chess> Source.
+ */
 
+namespace Gem\Controller\Auth;
+
+use Symfony\Component\HttpFoundation\Response;
+use function render;
+
+/**
+ * The initial controller for when someone visits
+ * the website at the doc root.
+ */
+class LoginController
+{
+    /**
+     * Return the response.
+     *
+     * @return mixed The controller response.
+     */
+    public function view()
+    {
+    }
+}

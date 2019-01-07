5cdbfc25e77d543ef8e796d8aa3a55cf45953f97
izejuy <46389306+izejuy@users.noreply.github.com>
2019-01-07 00:56:04 -0500

    Update version.php

--- version.php
+++ version.php
@@ -1,1 +1,9 @@
+<?php
+declare(strict_types=1);
+/**
+ * @author Gem Chess Contributors <https://github.com/izejuy/gem-chess/graphs/contributors>.
+ * @link <https://github.com/izejuy/gem-chess> Source.
+ */
 
+define('GEM_VERSION_ID', 100000);
+define('GEM_VERSION', 'v1.0.0')

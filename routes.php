b054dbb4b3bcf860bb62ef11af2f9581e4f45d48
izejuy <46389306+izejuy@users.noreply.github.com>
2019-01-07 00:53:18 -0500

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,3 +1,14 @@
+<?php
+declare(strict_types=1);
+/**
+ * @author Gem Chess Contributors <https://github.com/izejuy/gem-chess/graphs/contributors>.
+ * @link <https://github.com/izejuy/gem-chess> Source.
+ */
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Routing\Loader\YamlFileLoader;
+
+// Load the route yaml config file.
+$fileLocator = new FileLocator(array(__DIR__));
+$loader = new YamlFileLoader($fileLocator);
+$routes = $loader->load('routes.yaml');

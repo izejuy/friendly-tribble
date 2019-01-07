16feebd134aa353ec255ba22b2261f691bcaaf1d
izejuy <46389306+izejuy@users.noreply.github.com>
2019-01-07 01:01:49 -0500

    Update index.html.php

--- lib/views/index.html.php
+++ lib/views/index.html.php
@@ -1,1 +1,9 @@
-<?php $view->extend('/partials/main.html.php') ?>
+<!DOCTYPE html>
+<html lang="%">
+    <?php $view->extend('/partials/head.html.php') ?>
+    <body>
+        <h1>Hello world!</h1>
+        
+        <?php $view->extend('/partials/script.html.php') ?>
+    </body>
+</html>

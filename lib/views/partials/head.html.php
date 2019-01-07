b28dc148fe8aca25ee6a84056dece8995862b08a
izejuy <46389306+izejuy@users.noreply.github.com>
2019-01-07 01:05:39 -0500

    Update head.html.php

--- lib/views/partials/head.html.php
+++ lib/views/partials/head.html.php
@@ -1,9 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.2.1/superhero/bootstrap.min.css" rel="stylesheet" integrity="sha384-3C9izce+NThLmHHj0Gt0VSOFFr0FCxoyLNcCB+uYxTGXDdw64SxJ34SI/rnKRnvu" crossorigin="anonymous">  
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <?php $view->extend('/style.html.php') ?>
     <title><?php $view['slots']->output('title', 'Gem Chess - A better playing chess website powered by Symfony.') ?></title>
     <meta name="author" content="Gem Chess Contributors <https://github.com/izejuy/gem-chess/graphs/contributors>">
     <meta name="Mix" content="symfony-mix:cakephp-mix:crystal-mix">

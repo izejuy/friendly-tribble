9993d6e12472fb6b82670df1fd86154eee0a935c
izejuy <46389306+izejuy@users.noreply.github.com>
2019-01-11 23:32:50 -0500

    Create Rand.php

---
+++ src/Rand.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+/**
+ * Gem.
+ *
+ * A simple PHP content management system powered by Symfony and CakePHP.
+ *
+ * @author Gem Contributors <https://github.com/izejuy/gem/graphs/contributors>.
+ *
+ * @license MIT - A short and simple permissive license with conditions only requiring preservation of copyright and license notices.
+ *                Licensed works, modifications, and largerworks may be distributed under different terms and without source code.
+ *
+ * @link <https://github.com/izejuy/gem/blob/master/LICENSE> MIT License.
+ *
+ * @link <https://github.com/izejuy/gem> Source.
+ */
+
+namespace Izejuy\Gem;
+
+use function implode;
+use function mb_strlen;
+use function random_int;
+
+/**
+ * The rand class.
+ */
+class Rand implements RandInterface
+{
+
+    /**
+     * Generate a random bool value.
+     *
+     * @return bool Returns a random bool value.
+     */
+    public function getBool(): bool
+    {
+        return (bool) random_int(0, 1);
+    }
+
+    /**
+     * Generate a random string value.
+     *
+     * @param int    $length   The string length.
+     * @param string $keyspace The list of characters allowed to use.
+     *
+     * @return string Returns the generated string.
+     */
+    public function getString(int $length = 16, string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'): string
+    {
+        $pieces = [];
+        $max = mb_strlen($keyspace, '8bit') - 1;
+        for ($i = 0; $i < $length; $i++) {
+            $pieces[] = $keyspace[random_int(0, $max)];
+        }
+        return implode('', $pieces);
+    }
+
+    /**
+     * Generate a random two factor code.
+     *
+     * @param int $length The code length.
+     *
+     * @return int Returns the two factor code.
+     *
+     * @codeCoverageIgnore
+     */
+    public function getTwoFactorCode(int $length = 6): int
+    {
+        return intval($this->getString($length, '0123456789'));
+    }
+}
